af6011adaefadd9782abd4e233c1804b03420ffc
Prince Sofwan <sofwancoder@gmail.com>
2022-05-18 01:17:36 +0100

    chore: add internal logger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,17 @@
-import app from './application';
+import app from "./application";
+import { createServer } from "http";
+import { sequelize } from "./sequelize";
+import logger from "./internal/logger";
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  // tslint:disable-next-line:no-console
-  console.log(`Server started at http://localhost:${port}`);
-});
+void (async () => {
+  await sequelize.authenticate({
+    logging: () => {
+      logger.info("Database Authenticated");
+    },
+  });
+  createServer(app).listen(port, () =>
+    logger.info(`Server running on port ${port}`)
+  );
+})();
